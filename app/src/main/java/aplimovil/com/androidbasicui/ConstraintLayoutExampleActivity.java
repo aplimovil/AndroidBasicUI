af51ae4b37e20d2ec1c9a218ec09219b227a3961
fomarti <fomarti@gmail.com>
2019-04-04 10:17:28 -0500

    Add ConstraintLayout example

---
+++ app/src/main/java/aplimovil/com/androidbasicui/ConstraintLayoutExampleActivity.java
@@ -0,0 +1,13 @@
+package aplimovil.com.androidbasicui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintLayoutExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout_example);
+    }
+}
