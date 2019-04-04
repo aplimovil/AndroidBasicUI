d7cd75266d56e425967f46f0fe690bc6378c4d4b
fomarti <fomarti@gmail.com>
2019-04-04 08:48:57 -0500

    Add RelativeLayout example

---
+++ app/src/main/java/aplimovil/com/androidbasicui/RelativeLayoutExampleActivity.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * Applications development for mobile devices
+ * -------------------------------------------
+ *
+ * This RelativeLayoutExample implements an example that shows
+ * how to use a Relative Layout in Android.
+ *
+ * @author Francisco Martinez
+ *
+ *
+ */
+
+
+package aplimovil.com.androidbasicui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayoutExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_example);
+    }
+}
